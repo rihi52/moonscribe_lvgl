207efb850257ff753388615c9cc87b85481cd407
rihi52 <rileyhire7@gmail.com>
2026-07-05 07:22:40 -0400

    added combat button and flex layout to home screen

--- src/screens/home_screen.c
+++ src/screens/home_screen.c
@@ -21,10 +21,20 @@ lv_obj_t * pHomeScreen;
 void gvHome_screen(void) {
     pHomeScreen = lv_obj_create(NULL);
     lv_obj_add_style(pHomeScreen, &gHomeScreenStyle, 0);
+    lv_obj_set_align(pHomeScreen, LV_ALIGN_CENTER);
+    lv_obj_set_flex_flow(pHomeScreen, LV_FLEX_FLOW_COLUMN_WRAP);
+    lv_obj_set_style_flex_main_place(pHomeScreen, LV_FLEX_ALIGN_CENTER, 0);
+    lv_obj_set_style_flex_cross_place(pHomeScreen, LV_FLEX_ALIGN_CENTER, 0);
+    lv_obj_set_style_flex_track_place(pHomeScreen, LV_FLEX_ALIGN_CENTER, 0);
 
     lv_screen_load(pHomeScreen);
 
     lv_obj_t *pHomeScreenHeadLabel = lv_label_create(pHomeScreen);
     lv_label_set_text(pHomeScreenHeadLabel, "MOONSCRIBE");
     lv_obj_center(pHomeScreenHeadLabel);
+
+    lv_obj_t *pCombatButton = lv_button_create(pHomeScreen);
+    lv_obj_t *pCombatButtonLabel = lv_label_create(pCombatButton);
+    lv_obj_center(pCombatButtonLabel);
+    lv_label_set_text(pCombatButtonLabel, "Combat");
 }
